f91baf037361c9bfa4985f58870d75d2da8c9563
Ryan Yates <Ryan.yates@kilasuit.org>
2018-03-09 18:00:16 -0800

    Update Appveyor

--- build.ps1
+++ build.ps1
@@ -3,10 +3,10 @@
 #Requires -Modules @{ModuleName='Pester';ModuleVersion='4.1.1'}
 
 $Script:IsAppveyor = $env:APPVEYOR -ne $null
-$Script:ModuleName = 'PesterHelpers'
+$Script:ModuleName = Get-Item -Path $BuildRoot | Select-Object -ExpandProperty Name
 Get-Module -Name $ModuleName,'helpers' | Remove-Module -Force
 Import-Module "$BuildRoot\buildhelpers\helpers.psm1"
-#$tests = Get-ChildItem .\Pr*\*,.\Pu*\* -Recurse | Where-Object Name -Match Tests.ps1    
+if ($env:test) {$test = $env:test} else {$test = 'Min'}    
 
 
 #region Setup
@@ -28,7 +28,8 @@ task Clean {
 
 task TestCode {
     Write-Build Yellow "`n`n`nTesting dev code before build"
-    $TestResult = Invoke-Pester -Script $BuildRoot\$($ModuleName).$($env:Test).tests.ps1 -PassThru
+    Write-Build Yellow "Using the $test TestSuite"
+    $TestResult = Invoke-Pester -Script $BuildRoot\$($ModuleName).$($Test).tests.ps1 -PassThru
     if($TestResult.FailedCount -gt 0) {throw 'Tests failed'}
 }
 
@@ -36,54 +37,7 @@ task CopyFiles {
     $null = New-Item -Path "$BuildRoot\bin\$ModuleName" -ItemType Directory
     Copy-Item -Path "$BuildRoot\*.psd1" -Destination "$BuildRoot\bin\$ModuleName"
     Get-ChildItem -Path "$BuildRoot\license*" | Copy-Item -Destination "$BuildRoot\bin\$ModuleName"
-    Copy-Item $BuildRoot\Other -Destination $BuildRoot\$ModuleName\Other -Recurse
-}
-
-task CompilePSM {
-    $PrivatePath = '{0}\Private\*.ps1' -f $BuildRoot
-    $PublicPath = '{0}\Public\*.ps1'-f $BuildRoot
-    $ScriptPath = '{0}\Script\*.ps1'-f $BuildRoot
-    Merge-ModuleFiles -Path $ScriptPath,$PrivatePath,$PublicPath -OutputPath "$BuildRoot\bin\$ModuleName\$ModuleName.psm1"
-
-    $PublicScriptBlock = Get-ScriptBlockFromFile -Path $PublicPath
-    $PublicFunctions = Get-FunctionFromScriptblock -ScriptBlock $PublicScriptBlock
-    $PublicAlias = Get-AliasFromScriptblock -ScriptBlock $PublicScriptBlock
-    $PublicFunctionParam, $PublicAliasParam = ''
-    $UpdateManifestParam = @{}
-    if(-Not [String]::IsNullOrEmpty($PublicFunctions)) {
-        $PublicFunctionParam = "-Function '{0}'" -f ($PublicFunctions -join "','")
-        $UpdateManifestParam['FunctionsToExport'] = $PublicFunctions
-    }
-    if($PublicAlias) {
-        $PublicAliasParam = "-Alias '{0}'" -f ($PublicAlias -join "','")
-        $UpdateManifestParam['AliasesToExport'] = $PublicAlias
-    }
-    $ExportStrings = 'Export-ModuleMember',$PublicFunctionParam,$PublicAliasParam | Where-Object {-Not [string]::IsNullOrWhiteSpace($_)}
-    $ExportStrings -join ' ' | Out-File -FilePath  "$BuildRoot\bin\$ModuleName\$ModuleName.psm1" -Append -Encoding UTF8
-
-    # If we have git and gitversion installed, let's use it to get new module version and Release Notes
-    if ($(try{Get-Command -Name gitversion -ErrorAction Stop}catch{})) {
-        $gitversion = gitversion | ConvertFrom-Json
-        if ($gitversion.CommitsSinceVersionSource -gt 0) {
-            # Prerelease, raise minor-version by 1 and add prerelease string.
-            $UpdateManifestParam['ModuleVersion'] = '{0}.{1}.{2}' -f $gitversion.Major, ($gitversion.Minor), $gitversion.Patch
-            $UpdateManifestParam['Prerelease'] = '-beta{0}' -f $gitversion.CommitsSinceVersionSourcePadded
-        }
-        else {
-            # This is a release version
-            # If there is a tag pointing at HEAD, use that as release notes
-            $UpdateManifestParam['ModuleVersion'] = $gitversion.MajorMinorPatch
-            if ($(try{Get-Command -Name git -ErrorAction Stop}catch{})) {
-                if($CurrentTag = git tag --points-at HEAD) {
-                    $ReleaseNotes = git tag -l -n20 $CurrentTag | Select-Object -Skip 1
-                    $UpdateManifestParam['ReleaseNotes'] = $ReleaseNotes
-                }
-            }
-        }
-    }
-    if ($UpdateManifestParam.Count -gt 0) {
-        Update-ModuleManifest -Path "$BuildRoot\bin\$ModuleName\$ModuleName.psd1" @UpdateManifestParam
-    }
+    Copy-Item $BuildRoot\Other -Destination $BuildRoot\bin\$ModuleName\Other -Recurse
 }
 
 task MakeHelp -if (Test-Path -Path "$PSScriptRoot\Docs") {
@@ -102,20 +56,16 @@ task TestBuild {
     if($TestResult.FailedCount -gt 0) {throw 'Tests failed'}
 }
 
-
 task MakePSM1 {
 
     Get-ChildItem .\Scripts\init.ps1 | Out-File .\bin\$($ModuleName)\$($ModuleName).psm1 -Encoding utf8 
 
     $Private,$Public | ForEach-Object { Get-Content $_.FullName | Out-File .\bin\$($ModuleName)\$($ModuleName).psm1 -Encoding utf8 -Append }
-
-}
-task UpdateManifest {
-
+   
     $PublicScriptBlock = Get-ScriptBlockFromFile -Path $public 
     $PublicFunctions = Get-FunctionFromScriptblock -ScriptBlock $PublicScriptBlock
     $PublicAlias = Get-AliasFromScriptblock -ScriptBlock $PublicScriptBlock
-
+    
     $UpdateManifestParam = @{}
     if(-Not [String]::IsNullOrEmpty($PublicFunctions)) {
         $PublicFunctionParam = "-Function '{0}'" -f ($PublicFunctions -join "','")
@@ -155,5 +105,5 @@ task UpdateManifest {
 
 task . Clean, TestCode, Build
 
-task Build CopyFiles, MakePSM1, MakeHelp, UpdateManifest
+task Build CopyFiles, MakePSM1, MakeHelp    
 
