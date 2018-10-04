0ec7e0bdf51fadaeb29ea3177fa194b1b44cfd8f
Ryan Yates <Ryan@blackmarble.com>
2018-10-04 20:41:20 +0100

    Changed build script

--- build.ps1
+++ build.ps1
@@ -1,7 +1,9 @@
+Install-module InvokeBuild,Pester,PowerShellGet
+<#>
 #Requires -Modules @{ModuleName='InvokeBuild';ModuleVersion='3.2.1'}
 #Requires -Modules @{ModuleName='PowerShellGet';ModuleVersion='1.6.0'}
 #Requires -Modules @{ModuleName='Pester';ModuleVersion='4.1.1'}
-
+#>
 $Script:IsAppveyor = $env:APPVEYOR -ne $null
 $Script:ModuleName = Get-Item -Path $BuildRoot | Select-Object -ExpandProperty Name
 Get-Module -Name $ModuleName,'helpers' | Remove-Module -Force
